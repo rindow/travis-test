92691ca1afd27252a1de4a74d4d331d036734c9b
Yuichi Ishikawa <yuichiis.pub@gmail.com>
2018-11-23 21:05:54 +0900

    multi version and test apc

--- Test.php
+++ Test.php
@@ -4,8 +4,18 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
-	public function testTravis()
-	{
-		$this->assertTrue(true);
-	}
+    public function testTravis()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testApc()
+    {
+        $this->assertTrue(extension_loaded('apc'));
+    }
+
+    public function testApcu()
+    {
+        $this->assertTrue(extension_loaded('apcu'));
+    }
 }
