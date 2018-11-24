09f2e054a0bf344e39629a74d357640e59e7e18f
Yuichi Ishikawa <yuichiis.pub@gmail.com>
2018-11-24 21:56:08 +0900

    FileLocator test

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-if(!class_exists('PHPUnit\Framework\TestCase')) {
-    include __DIR__.'/travis/patch55.php';
-}
 if(file_exists(__DIR__.'/../vendor/autoload.php')) {
 	include __DIR__.'/../vendor/autoload.php';
+}
+if(!class_exists('PHPUnit\Framework\TestCase')) {
+    include __DIR__.'/travis/patch55.php';
 }
--- tests/Tests/Test.php
+++ tests/Tests/Test.php
@@ -1,9 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Rindow\Stdlib\FileUtil\FileLocator;
 
 class Test extends TestCase
 {
+    static $RINDOW_TEST_RESOURCES;
+    public static function setUpBeforeClass()
+    {
+        self::$RINDOW_TEST_RESOURCES = __DIR__.'/../resources';
+    }
+
     public function testTravis()
     {
         $this->assertTrue(true);
@@ -31,5 +38,16 @@ class Test extends TestCase
     public function testComposer()
     {
         $this->assertTrue(interface_exists('Interop\\Lenient\\Annotation\\AnnotationReader'));
+    }
+
+    public function testGetAllClassNames()
+    {
+        $paths = array('Foo\\Bar'=>self::$RINDOW_TEST_RESOURCES.'/Stdlib/FileUtil/filelocator/foobar');
+        $filelocator = new FileLocator($paths,'.orm.yml');
+        $classNames = $filelocator->getAllClassNames('GlobalBaseName');
+var_dump($classNames);
+        $this->assertContains('Foo\Bar\ClassA',$classNames);
+        $this->assertContains('Foo\Bar\Sub\ClassB',$classNames);
+        $this->assertNotContains('Foo\Bar\Sub\GlobalBaseName',$classNames);
     }
 }
