7ef858e46781afb3530269a6767609c52a04a00a
Yuichi Ishikawa <yuichiis.pub@gmail.com>
2018-11-26 20:03:46 +0900

    test pgsql xa

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,6 +2,12 @@
 if(file_exists(__DIR__.'/../vendor/autoload.php')) {
 	include __DIR__.'/../vendor/autoload.php';
 }
+define('RINDOW_TEST_CLEAR_CACHE_INTERVAL',100000);
+
+define('RINDOW_TEST_PGSQL_USER','postgres');
+define('RINDOW_TEST_PGSQL_PASSWORD','password');
+define('RINDOW_TEST_PGSQL_DBNAME','postgres');
+
 if(!class_exists('PHPUnit\Framework\TestCase')) {
     include __DIR__.'/travis/patch55.php';
 }
--- tests/Tests/Test.php
+++ tests/Tests/Test.php
@@ -2,6 +2,9 @@
 
 use PHPUnit\Framework\TestCase;
 use Rindow\Stdlib\FileUtil\FileLocator;
+use Rindow\Database\Pdo\Transaction\Xa\DataSource as XaDataSource;
+use Rindow\Transaction\Distributed\Xid;
+use Interop\Lenient\Transaction\Xa\XAResource as XAResourceInterface;
 
 class Test extends TestCase
 {
@@ -9,6 +12,57 @@ class Test extends TestCase
     public static function setUpBeforeClass()
     {
         self::$RINDOW_TEST_RESOURCES = __DIR__.'/../resources';
+    }
+
+    public static function getPDOClientStaticPgsql()
+    {
+        $dsn = "pgsql:host=127.0.0.1;dbname=".RINDOW_TEST_PGSQL_DBNAME;
+        $username = RINDOW_TEST_PGSQL_USER;
+        $password = RINDOW_TEST_PGSQL_PASSWORD;
+        $options  = array();
+        $client = new \PDO($dsn, $username, $password, $options);
+        return $client;
+    }
+    public function getPDOClientPgsql()
+    {
+        return self::getPDOClientStaticPgsql();
+    }
+
+    public function rollbackAllPgsql()
+    {
+        $config = array(
+            'dsn' => 'pgsql:host=127.0.0.1;dbname='.RINDOW_TEST_PGSQL_DBNAME,
+            'user'     => RINDOW_TEST_PGSQL_USER,
+            'password' => RINDOW_TEST_PGSQL_PASSWORD,
+        );
+        $dataSource = new XaDataSource($config);
+        $connection = $dataSource->getConnection();
+        $xaResource = $connection->getXAResource();
+        $xids = $xaResource->recover(0);
+        foreach ($xids as $xid) {
+            $xaResource->rollback($xid);
+        }
+        $connection->close();
+    }
+    public function countRowPgsql()
+    {
+        $client = $this->getPDOClientPgsql();
+        $stmt = $client->query("SELECT * FROM testdb");
+        $c = 0;
+        foreach ($stmt as $row) {
+            $c += 1;
+        }
+        return $c;
+    }
+    public function setUpPgsql()
+    {
+        usleep( RINDOW_TEST_CLEAR_CACHE_INTERVAL );
+        \Rindow\Stdlib\Cache\CacheFactory::clearCache();
+        usleep( RINDOW_TEST_CLEAR_CACHE_INTERVAL );
+        $this->rollbackAllPgsql();
+        $client = $this->getPDOClientPgsql();
+        $client->exec("DROP TABLE IF EXISTS testdb");
+        $client->exec("CREATE TABLE testdb ( id SERIAL PRIMARY KEY , name TEXT , day DATE, ser INTEGER UNIQUE)");
     }
 
     public function testTravis()
@@ -45,9 +99,42 @@ class Test extends TestCase
         $paths = array('Foo\\Bar'=>self::$RINDOW_TEST_RESOURCES.'/Stdlib/FileUtil/filelocator/foobar');
         $filelocator = new FileLocator($paths,'.orm.yml');
         $classNames = $filelocator->getAllClassNames('GlobalBaseName');
-var_dump($classNames);
         $this->assertContains('Foo\Bar\ClassA',$classNames);
         $this->assertContains('Foo\Bar\Sub\ClassB',$classNames);
         $this->assertNotContains('Foo\Bar\Sub\GlobalBaseName',$classNames);
+    }
+
+    public function testXAResourcePgsqlCommitNormal()
+    {
+        $this->setUpPgsql();
+
+        $config = array(
+            'dsn' => 'pgsql:host=127.0.0.1;dbname='.RINDOW_TEST_PGSQL_DBNAME,
+            'user'     => RINDOW_TEST_PGSQL_USER,
+            'password' => RINDOW_TEST_PGSQL_PASSWORD,
+        );
+        $dataSource = new XaDataSource($config);
+        $connection = $dataSource->getConnection();
+        try {
+            $xaResource = $connection->getXAResource();
+    
+            $xid = new Xid('foo');
+            $xaResource->start($xid,XAResourceInterface::TMNOFLAGS);
+            $connection->exec("INSERT INTO testdb (name) VALUES ('aaa') ");
+            $xaResource->end($xid,XAResourceInterface::TMSUCCESS);
+            $this->assertEquals(XAResourceInterface::XA_OK,$xaResource->prepare($xid));
+            $result = $xaResource->recover(0);
+            $this->assertEquals(1,count($result));
+            $this->assertEquals('foo',$result[0]->getGlobalTransactionId());
+            $this->assertEquals(0,$this->countRowPgsql());
+            $xaResource->commit($xid,false);
+            $this->assertEquals(1,$this->countRowPgsql());
+        } catch(\Exception $e) {
+            echo get_class($e).':'.$e->getMessage()."\n";
+            $xaResource->rollback($xid);
+            $connection->close();
+            echo 'close';
+            throw $e;
+        }
     }
 }
